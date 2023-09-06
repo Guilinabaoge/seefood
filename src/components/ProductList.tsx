d25931d9c8101b1e52ec3692170aa8748c0bf45e
yuhu0006 <yuhu0006@gmail.com>
2023-09-07 00:25:34 +0200

    Add style to productList

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,35 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import Product from "./Product";
+import Product from "../components/Product";
 
-function ProductList(props:any){
-    const [items,setItems] = useState([]);
+function ProductList(props: any) {
+    const [items, setItems] = useState([]);
 
     useEffect(() => {
-        if(props.currentStore!=null){
-            axios.get(`https://api.sallinggroup.com/v1/food-waste/${props.currentStore.id}`,{
+        if (props.currentStore != null) {
+            axios.get(`https://api.sallinggroup.com/v1/food-waste/${props.currentStore.id}`, {
                 headers: {
                     Authorization: `Bearer ${process.env.REACT_APP_SALLING_API_KEY}`
-                }}).then(response => {
-                    setItems(response.data.clearances);
-                })
+                }
+            }).then(response => {
+                setItems(response.data.clearances);
+            })
         }
-    },[props.currentStore]);
+    }, [props.currentStore]);
 
     return (
-        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center'}}>
-        {items.map((item:any,index:any)=>{
+        <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+            {items.map((item: any, index: any) => {
 
-                return item.product.categories?.da ?(
+                return item.product.categories?.da ? (
                     <div key={index}
-                        style={{ width: '33.33%', padding: '10px' ,border: '1px solid black'}}>
-                        <Product item={item}/>
+                       className={"productList"}>
+                        <Product item={item} />
                     </div>
                 ) : <></>;
-            
-        }
-        )}
-        </div>    
+
+            }
+            )}
+        </div>
     )
 }
 
